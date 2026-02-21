9b85b4842e9fbf3b525868c140e1e2f319501d34
Sanghoon Lee <gcccompil3r@gmail.com>
2026-02-21 18:33:06 +0900

    feat: [EDDI] 이메일 정보로 가입 / 미가입 여부 감지 [ETB-2]

---
+++ src/main/java/com/example/edditcghomespring/account/domain/repository/AccountProfileRepository.java
@@ -0,0 +1,10 @@
+package com.example.edditcghomespring.account.domain.repository;
+
+import com.example.edditcghomespring.account.domain.entity.AccountProfile;
+import com.example.edditcghomespring.account.domain.vo.Email;
+
+import java.util.Optional;
+
+public interface AccountProfileRepository {
+    Optional<AccountProfile> findByEmail(Email email);
+}
